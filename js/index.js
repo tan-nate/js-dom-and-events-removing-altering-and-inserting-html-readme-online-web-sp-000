42581ee9e85852af580f2d3fe85bf9f4d865fe8a
Nate Tan <nktan93@gmail.com>
2019-09-09 20:41:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+let element = document.createElement('div');
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+document.body.appendChild(element);
+element.style.textAlign = 'center';
